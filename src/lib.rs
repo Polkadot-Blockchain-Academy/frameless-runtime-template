c9b95cbf796b8a43a70fb15db0f9b7bd49ed7fc3
Shawn Tabrizi <shawntabrizi@gmail.com>
2025-08-31 09:27:16 +0800

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -81,7 +81,7 @@
 //! Can you guess what is this?
 //!
 //! ```text
-//! wscat -c 127.0.0.1:9944 -x '{"jsonrpc":"2.0", "id":1, "method":"state_getStorage", "params": ["3a636f6465"] }
+//! wscat -c 127.0.0.1:9944 -x '{"jsonrpc":"2.0", "id":1, "method":"state_getStorage", "params": ["3a636f6465"] }'
 //! ```
 //!
 //! If you want to try submitting a transaction, you can use the following:
